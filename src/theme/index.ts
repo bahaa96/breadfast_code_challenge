3aceb7506c0b63e16535978db63b635e8bd5fc9b
Ahmed Bahaa <geekbahaa1@gmail.com>
2024-07-28 12:00:23 +0300

    Adding a global theme

---
+++ src/theme/index.ts
@@ -0,0 +1,43 @@
+const lightTheme = {
+  colors: {
+    primary: '#A90282',
+    surface: '#F0F2F5',
+    surfaceHighlight: '#fff',
+    secondary: '#79ABB2',
+    textPrimary: '#A90282',
+    textColor: '#000',
+    textInverse: '#fff',
+    borderColor: '#000',
+    danger: 'red500',
+    info: 'gray500',
+    infoText: 'gray600',
+  },
+  components: {
+    Text: {
+      color: '#000',
+    },
+  },
+};
+
+const darkTheme = {
+  colors: {
+    primary: '#A90282',
+    surface: '#181A1C',
+    surfaceHighlight: '#000000',
+    secondary: '#79ABB2',
+    textPrimary: '#A90282',
+    textColor: '#fff',
+    textInverse: '#4E4E4E',
+    borderColor: '#fff',
+    danger: 'red500',
+    info: 'gray500',
+    infoText: 'gray600',
+  },
+  components: {
+    Text: {
+      color: '#fff',
+    },
+  },
+};
+
+export {lightTheme, darkTheme};
